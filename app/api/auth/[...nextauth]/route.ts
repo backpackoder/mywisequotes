45ef22ec12c783c19c45baafa807989d77d40160
harderth <th.barbiera@outlook.fr>
2023-06-20 22:03:41 -0600

    changing an import

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 import type { NextAuthOptions } from "next-auth";
 import Google from "next-auth/providers/google";
 import Facebook from "next-auth/providers/facebook";
