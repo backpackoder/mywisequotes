d6075b32e53325c78d9b5a75fb7cebc7a7c21fd5
harderth <th.barbiera@outlook.fr>
2024-02-09 04:34:32 +0100

    Fix minor mistake

--- utils/getRole.tsx
+++ utils/getRole.tsx
@@ -3,20 +3,12 @@ import Image from "next/image";
 // Assets
 import moderator from "@/app/assets/images/roles/moderator.png";
 import admin from "@/app/assets/images/roles/admin.png";
-import owner from "@/app/assets/images/roles/owner.png";
 
 // Types
 import { Role } from "@prisma/client";
 
 export function getRole(role: Role) {
   switch (role) {
-    case "OWNER":
-      return (
-        <>
-          <Image src={owner} alt="Owner's badge" width={50} height={50} /> Owner
-        </>
-      );
-
     case "ADMIN":
       return (
         <>
